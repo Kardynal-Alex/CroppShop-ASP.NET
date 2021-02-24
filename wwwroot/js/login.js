e19a105dbb534a9367e12b955a8774fac1babf91
Kardynal-Alex <alexandrkardynal@gmail.com>
2021-02-24 15:15:26 +0200

    identity login/register form

---
+++ wwwroot/js/login.js
@@ -0,0 +1,30 @@
+﻿function myFunction() {
+    var x = document.getElementById("login-form");
+    var y = document.getElementById("regist-but");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+        y.style.display = "block";
+    } else {
+        x.style.display = "none";
+        y.style.display = "none";
+    }
+    var x = document.getElementById("regist-form");
+    var y = document.getElementById("log-but");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+        y.style.display = "block";
+    } else {
+        x.style.display = "none";
+        y.style.display = "none";
+    }
+}
+function showPwd(id, el) {
+    let x = document.getElementById(id);
+    if (x.type === "password") {
+        x.type = "text";
+        el.className = 'fa fa-eye-slash showpwd';
+    } else {
+        x.type = "password";
+        el.className = 'fa fa-eye showpwd';
+    }
+}
